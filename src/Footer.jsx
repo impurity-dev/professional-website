6bbeb2d8fdc1b76c7e5dba6e8dfad8bff9d3fe88
Tyler Kokoszka <techkoko77@gmail.com>
2020-08-04 00:41:36 -0400

    Setup skill levels

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,7 +2,23 @@ import React from 'react';
 import './Footer.scss';
 
 function Footer() {
-    return <div className="d-flex flex-row">TODO</div>;
+    return (
+        <div className="d-flex flex-row">
+            <ul>
+                <li>
+                    Icons made by{' '}
+                    <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
+                        Freepik
+                    </a>{' '}
+                    from{' '}
+                    <a href="https://www.flaticon.com/" title="Flaticon">
+                        {' '}
+                        www.flaticon.com
+                    </a>
+                </li>
+            </ul>
+        </div>
+    );
 }
 
 export default Footer;
--- src/Skill.jsx
+++ src/Skill.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Skill.scss';
+import { ReactComponent as FilledStar } from './assets/FilledStar.svg';
+import { ReactComponent as EmptyStar } from './assets/EmptyStar.svg';
 
-function Skill({ name, rating }) {
+function Skill({ name, rating, maxRating }) {
+    const stars = [];
+    for (let i = 0; i < rating; i++) {
+        stars.push(<FilledStar className="text-primary-8" width={24} key={i} />);
+    }
+    const emptyStars = maxRating - rating;
+    for (let i = 0; i < emptyStars; i++) {
+        stars.push(<EmptyStar className="text-primary-8" width={24} key={i} />);
+    }
     return (
         <div className="d-flex flex-row">
-            <div>{name}</div>
-            <div>{rating}/10</div>
+            <div className="flex-grow-1">{name}</div>
+            <div>{stars}</div>
         </div>
     );
 }
@@ -14,6 +24,11 @@ function Skill({ name, rating }) {
 Skill.propTypes = {
     name: PropTypes.string.isRequired,
     rating: PropTypes.number.isRequired,
+    maxRating: PropTypes.number,
+};
+
+Skill.defaultProps = {
+    maxRating: 10,
 };
 
 export default Skill;
