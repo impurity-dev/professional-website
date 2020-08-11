1c185abcbb9430eaae98501884cf91be4a2e771b
Tyler Kokoszka <techkoko77@gmail.com>
2020-08-11 16:34:35 -0400

    Update footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,20 +3,12 @@ import './Footer.scss';
 
 function Footer() {
     return (
-        <div className="d-flex flex-row">
-            Icons made by{' '}
-            <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
-                Freepik
-            </a>
-            {', '}
-            <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">
-                Pixel perfect
-            </a>{' '}
-            from{' '}
-            <a href="https://www.flaticon.com/" title="Flaticon">
-                {' '}
-                www.flaticon.com
-            </a>
+        <div className="d-flex flex-row px-5">
+            <p>
+                Icons made by <a href="https://www.flaticon.com/authors/freepik">Freepik</a>,{' '}
+                <a href="https://www.flaticon.com/authors/pixel-perfect">Pixel perfect</a> from{' '}
+                <a href="https://www.flaticon.com/">www.flaticon.com</a>
+            </p>
         </div>
     );
 }
