ab5757cb45154cacfd7e18baeddcae4315efacf4
Tyler Kokoszka <techkoko77@gmail.com>
2020-08-11 17:09:02 -0400

    Added spinner to homescreen

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import './Home.scss';
 import Stars from './Stars';
 import RingMenu from './RingMenu';
+import { ReactComponent as HomeSpinner } from './assets/HomeSpinner.svg';
 
 function Home() {
     useEffect(() => {
@@ -14,9 +15,12 @@ function Home() {
 
     return (
         <>
+            <div className="position-fixed w-100 h-100 ">
+                <HomeSpinner className="w-100 h-100 text-cyan" />
+            </div>
             <Stars />
             <RingMenu className="d-flex flex-row justify-content-center w-100" />
-            <div className="d-flex flex-column justify-content-center position-fixed">
+            <div className="d-flex flex-column position-fixed">
                 <h1 className="text-white">Tyler Kokoszka</h1>
                 <hr />
                 <h2 className="text-white">Software Engineer</h2>
