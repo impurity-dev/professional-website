ad25c7a59e00f2406fc43a629b649e4c2c391d80
Tyler Kokoszka <techkoko77@gmail.com>
2020-08-10 20:42:42 -0400

    Added circular home menu

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import AboutMe from './AboutMe';
 import './Body.scss';
+import ExperienceData from './data/Experience.json';
+import SkillListData from './data/SkillList.json';
+import ExperienceList from './ExperienceList';
 import Home from './Home';
-import MainPanel from './MainPanel';
 import SidePanel from './SidePanel';
+import SkillList from './SkillList';
+
+const { skills } = SkillListData;
+const { experiences } = ExperienceData;
 
 function Body() {
     return (
@@ -11,7 +18,21 @@ function Body() {
             <Switch>
                 <Route path="/about">
                     <SidePanel className="w-25 mr-2" />
-                    <MainPanel className="w-75 ml-2" />
+                    <div className="d-flex flex-column p-5 shadow rounded bg-gray-2 w-75 ml-2">
+                        <AboutMe />
+                    </div>
+                </Route>
+                <Route path="/skills">
+                    <SidePanel className="w-25 mr-2" />
+                    <div className="d-flex flex-column p-5 shadow rounded bg-gray-2 w-75 ml-2">
+                        <SkillList skills={skills} />
+                    </div>
+                </Route>
+                <Route path="/experiences">
+                    <SidePanel className="w-25 mr-2" />
+                    <div className="d-flex flex-column p-5 shadow rounded bg-gray-2 w-75 ml-2">
+                        <ExperienceList experiences={experiences} />
+                    </div>
                 </Route>
                 <Route path="/">
                     <Home />
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,56 @@
 import React from 'react';
 import './Home.scss';
+import { Link } from 'react-router-dom';
+import { ReactComponent as BriefCase } from './assets/BriefCase.svg';
+import { ReactComponent as User } from './assets/User.svg';
 
 function Home() {
-    return <div className="d-flex flex-row">TODO HOME</div>;
+    return (
+        <div className="d-flex flex-row justify-content-center w-100">
+            <ul className="home-menu-container">
+                <li>
+                    <Link to="/about">
+                        <User className="text-primary-6 home-menu-item" width={100} />
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/experience">
+                        <BriefCase className="text-primary-6 home-menu-item" width={100} />
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/about">
+                        <User className="text-primary-6 home-menu-item" width={100} />
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/experience">
+                        <BriefCase className="text-primary-6 home-menu-item" width={100} />
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/about">
+                        <User className="text-primary-6 home-menu-item" width={100} />
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/experience">
+                        <BriefCase className="text-primary-6 home-menu-item" width={100} />
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/about">
+                        <User className="text-primary-6 home-menu-item" width={100} />
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/experience">
+                        <BriefCase className="text-primary-6 home-menu-item" width={100} />
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    );
 }
 
 export default Home;
