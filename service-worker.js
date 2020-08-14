126834d695afb74eba4544c8a9ab920b50a27d9f
impurity-dev <techkoko77@gmail.com>
2020-08-14 19:29:34 +0000

    Deploying to gh-pages from  @ 9fd62a8a72332daa766940fb5585129c559dbdba 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c68a9fc23a5af488c721df461c2300c2.js"
+  "/professional-website/precache-manifest.88fd4ffbf4428f3ab2756f0e24563abc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/professional-website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
