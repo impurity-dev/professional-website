9468d13ea638869175b3b6a15985c62787e552c6
Tyler Kokoszka <techkoko77@gmail.com>
2020-08-17 13:00:15 -0400

    Added webpack config

--- src/components/StarsV2.tsx
+++ src/components/StarsV2.tsx
@@ -3,7 +3,6 @@ import { Engine } from '@babylonjs/core/Engines/engine';
 import { Texture } from '@babylonjs/core/Materials/Textures/texture';
 import { Color4, Vector3 } from '@babylonjs/core/Maths/math';
 import '@babylonjs/core/Meshes/Builders/sphereBuilder';
-import { Mesh } from '@babylonjs/core/Meshes/mesh';
 import '@babylonjs/core/Meshes/meshBuilder';
 import { ParticleSystem } from '@babylonjs/core/Particles/particleSystem';
 import { Scene } from '@babylonjs/core/scene';
@@ -30,10 +29,6 @@ function createStars(id: string): Scene {
     const camera = new FreeCamera('Camera', new Vector3(0, 0, -10), scene);
     camera.setTarget(Vector3.Zero());
 
-    // Emitters
-    const particleEmitter = Mesh.CreateBox('Emitter', 0.1, scene);
-    particleEmitter.isVisible = false;
-
     const particleSystem = new ParticleSystem('Particles', 2000, scene);
     particleSystem.particleTexture = new Texture(StarTexture, scene);
     particleSystem.minAngularSpeed = -0.5;
@@ -42,15 +37,11 @@ function createStars(id: string): Scene {
     particleSystem.maxSize = 0.01;
     particleSystem.minLifeTime = 5;
     particleSystem.maxLifeTime = 6;
+    particleSystem.createBoxEmitter(new Vector3(1, 1, 1), new Vector3(-1, -1, -1), new Vector3(0, 0, 0), new Vector3(0, 0, 0));
     particleSystem.minEmitPower = 0.5;
     particleSystem.maxEmitPower = 4.0;
-    particleSystem.emitter = particleEmitter;
     particleSystem.emitRate = 400;
     particleSystem.blendMode = ParticleSystem.BLENDMODE_ONEONE;
-    particleSystem.minEmitBox = new Vector3(0, 0, 0);
-    particleSystem.maxEmitBox = new Vector3(0, 0, 0);
-    particleSystem.direction1 = new Vector3(-1, -1, -1);
-    particleSystem.direction2 = new Vector3(1, 1, 1);
     particleSystem.color1 = new Color4(1, 1, 1);
     particleSystem.color2 = new Color4(1, 1, 1);
     particleSystem.start();
