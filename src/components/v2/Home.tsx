f79644a3597cacad12feeb5f608822acac5da5a4
Tyler Kokoszka <techkoko77@gmail.com>
2020-08-18 15:06:56 -0400

    Cleaning up some extraneous containers

--- src/components/v2/Home.tsx
+++ src/components/v2/Home.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './Home.scss';
 import Stars from './Stars';
-import Hud from './Hud';
+import Cockpit from './Cockpit';
+import HudSphere from './HudSphere';
 
 type HomeProps = { className?: string };
 
@@ -9,7 +10,8 @@ function Home({ className = '' }: HomeProps): JSX.Element {
     return (
         <div id="space-container" className={`position-fixed w-100 h-100 ${className}`}>
             <Stars id="stars" className="position-absolute w-100 h-100" />
-            <Hud className="position-absolute" />
+            <Cockpit id="cockpit" className="position-absolute w-100 h-100" />
+            <HudSphere id="sphere-hud" className="position-absolute" size={200} />
         </div>
     );
 }
