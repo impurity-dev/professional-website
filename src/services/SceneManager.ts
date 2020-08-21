8c09b9453b6b46d3faa064c501b4d3797a0cd9d7
Tyler Kokoszka <techkoko77@gmail.com>
2020-08-21 01:08:48 -0400

    Fixed 3d hover issue and removed inspector from prod build

--- src/services/SceneManager.ts
+++ src/services/SceneManager.ts
@@ -1,11 +1,16 @@
 import { Scene } from '@babylonjs/core/scene';
-import '@babylonjs/inspector';
-import '@babylonjs/core/Debug/debugLayer';
+import '@babylonjs/core/Helpers/sceneHelpers'; // NOTE: This enables 3d mouse events
 
 export class SceneManager {
     public static attachInspector(scene: Scene): void {
         if (process.env.REACT_APP_DEBUG === 'true') {
-            scene.debugLayer.show();
+            import('@babylonjs/inspector')
+                .then(() =>
+                    import('@babylonjs/core/Debug/debugLayer')
+                        .then(() => (scene as any).debugLayer.show())
+                        .catch((e) => console.error(`Could not load debug layer: ${e}`)),
+                )
+                .catch((e) => console.error(`Could not load inspector: ${e}`));
         }
     }
 }
