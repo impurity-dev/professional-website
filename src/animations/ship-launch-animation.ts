18c7712054ad2af41c76475202b7f773487f616e
Tyler Kokoszka <techkoko77@gmail.com>
2020-12-22 10:26:10 -0500

    Fixed easing function on launch animation

--- src/animations/ship-launch-animation.ts
+++ src/animations/ship-launch-animation.ts
@@ -1,14 +1,14 @@
-import { Animation, BackEase, EasingFunction, Vector3 } from '@babylonjs/core';
+import { Animation, EasingFunction, SineEase, Vector3 } from '@babylonjs/core';
 
 export default class ShipLaunchAnimation extends Animation {
     constructor(private readonly startingPosition: Vector3, public readonly frameRate: number) {
         super('ShipLaunchAnimation', 'position', frameRate, Animation.ANIMATIONTYPE_VECTOR3, Animation.ANIMATIONLOOPMODE_CONSTANT);
-        const easeFunction = new BackEase();
+        const easeFunction = new SineEase();
         easeFunction.setEasingMode(EasingFunction.EASINGMODE_EASEIN);
         this.setEasingFunction(easeFunction);
         const keyFrames = [];
         keyFrames.push({ frame: 0, value: startingPosition });
-        keyFrames.push({ frame: frameRate, value: startingPosition.add(new Vector3(0, 0, 500)) });
+        keyFrames.push({ frame: frameRate, value: startingPosition.add(new Vector3(0, 0, 2500)) });
         this.setKeys(keyFrames);
     }
 }
