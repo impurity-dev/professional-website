2c923df3c06917c014aaf39b4f84603fa837bdce
Tyler Kokoszka <techkoko77@gmail.com>
2020-12-21 21:12:40 -0500

    Added func for colors and emissions

--- src/particles/gas-cloud-particles.ts
+++ src/particles/gas-cloud-particles.ts
@@ -9,19 +9,14 @@ export default class GasCloudParticles extends ParticleSystem {
         const fogTexture = new Texture('https://raw.githubusercontent.com/aWeirdo/Babylon.js/master/smoke_15.png', scene);
         this.fountain = Mesh.CreateBox('GasCloudsFountain', 0.01, scene);
         this.fountain.visibility = 0;
-
         this.minEmitBox = new Vector3(-100, -100, 100);
         this.maxEmitBox = new Vector3(100, 100, 100);
-
         this.particleTexture = fogTexture.clone();
         this.emitter = this.fountain;
         this.minSize = 35;
         this.maxSize = 100;
         this.minLifeTime = 10;
         this.maxLifeTime = 20;
-        this.preWarmCycles = 50;
-        this.preWarmStepOffset = 10;
-        this.preWarmCycles = 100;
         this.blendMode = ParticleSystem.BLENDMODE_STANDARD;
         this.gravity = new Vector3(0, 0, 0);
         this.direction1 = new Vector3(0, 0, -1);
@@ -31,15 +26,14 @@ export default class GasCloudParticles extends ParticleSystem {
         this.minEmitPower = 50;
         this.maxEmitPower = 100;
         this.updateSpeed = 0.05;
-        this.color1 = randomColor(null, null, null, 0);
+        this.setColorsAndEmit();
+        setInterval(() => this.setColorsAndEmit(), 50 * randomNumberBetween(100, 200));
+    }
+
+    private setColorsAndEmit(): void {
+        this.color1 = randomColor(null, null, null, 0.5);
         this.color2 = randomColor(null, null, null, 1);
         this.colorDead = randomColor(null, null, null, 0);
         this.manualEmitCount = randomNumberBetween(10, 75);
-        setInterval(() => {
-            this.color1 = randomColor(null, null, null, 0);
-            this.color2 = randomColor(null, null, null, 1);
-            this.colorDead = randomColor(null, null, null, 0);
-            this.manualEmitCount = randomNumberBetween(10, 75);
-        }, 50 * randomNumberBetween(100, 200));
     }
 }
