61e88f27b8c6e2cddde75628fef2a2d5802eb84c
Tyler Kokoszka <techkoko77@gmail.com>
2020-12-30 19:16:13 -0500

    Added planet orbit animation and orbit positioning

--- src/entities/planet-entity.ts
+++ src/entities/planet-entity.ts
@@ -1,11 +1,14 @@
-import { Color3, Mesh, MeshBuilder, Scene, Texture } from '@babylonjs/core';
+import { Color3, Mesh, Scene, Texture, TransformNode } from '@babylonjs/core';
 import { LavaMaterial } from '@babylonjs/materials';
 
-export default class PlanetEntity {
+export default class PlanetEntity extends TransformNode {
     public readonly sphere: Mesh;
 
     constructor(private readonly scene: Scene, diameter: number) {
-        const sphere: Mesh = MeshBuilder.CreateSphere('Sphere', { diameter });
+        super('Planet');
+        const sphere: Mesh = Mesh.CreateSphere('Planet Sphere', 100, diameter, scene);
+        sphere.parent = this;
+
         const lavaMaterial = new LavaMaterial('Lava', this.scene);
         lavaMaterial.noiseTexture = new Texture('https://www.babylonjs-playground.com/textures/lava/cloud.png', this.scene); // Set the bump texture
         lavaMaterial.diffuseTexture = new Texture('https://www.babylonjs-playground.com/textures/lava/lavatile.jpg', this.scene); // Set the diffuse texture
--- src/states/orbit-state.ts
+++ src/states/orbit-state.ts
@@ -1,19 +1,41 @@
-import { ArcRotateCamera, Color4, HemisphericLight, MeshBuilder, Scene, Vector3 } from '@babylonjs/core';
+import { ArcRotateCamera, Color4, HemisphericLight, Scene, Vector3 } from '@babylonjs/core';
+import PlanetEntity from '../entities/planet-entity';
+import SpaceShipEntity from '../entities/spaceship-entity';
 import OrbitGui from '../guis/orbit-gui';
 import SpaceSkybox from '../skyboxes/space-skybox';
 import State from './state';
 import TravelState from './travel-state';
 
 export default class Orbit extends State {
+    private spaceship: SpaceShipEntity;
+    private camera: ArcRotateCamera;
+
     async run(): Promise<void> {
         const engine = this.gameManager.engine;
         engine.displayLoadingUI();
         this.scene = new Scene(engine);
         this.scene.clearColor = new Color4(0, 0, 0, 1);
-        const camera: ArcRotateCamera = new ArcRotateCamera('Camera', Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), this.scene);
-        camera.setTarget(Vector3.Zero());
+        this.spaceship = new SpaceShipEntity(this.scene);
+        this.spaceship.position = Vector3.Zero();
+        this.camera = new ArcRotateCamera(
+            'ArcFollowCamera',
+            0,
+            Math.PI / 2.5,
+            100,
+            this.spaceship.position.add(new Vector3(0, 15, 50)),
+            this.scene,
+        );
+        this.scene.activeCamera = this.camera;
 
-        MeshBuilder.CreateSphere('sphere', { diameter: 1 }, this.scene);
+        const planetDiameter = 1000;
+        const planet = new PlanetEntity(this.scene, planetDiameter);
+        planet.position = this.spaceship.position.add(new Vector3(-(planetDiameter / 2 + 100), -50, -100));
+
+        let angle = 0;
+        this.scene.registerBeforeRender(function () {
+            planet.rotation.y = angle;
+            angle += 0.00025;
+        });
 
         new OrbitGui(this.scene, () => {
             this.goToTravel();
