ec016810b73d1a6c58fc089aab300fcab0490511
impurity-dev <techkoko77@gmail.com>
2023-09-05 21:59:15 -0400

    Fix vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,13 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({});
+export default defineConfig(({ command }) => {
+    if (command !== 'build') {
+        return {
+            base: '/',
+        };
+    } else {
+        return {
+            base: '/professional-website/',
+        };
+    }
+});
