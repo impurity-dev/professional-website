1f0f7777b779084f667a060f5ab370bb38fbb916
impurity-dev <techkoko77@gmail.com>
2023-11-15 21:57:47 -0500

    Working

--- src/environments/start-world.ts
+++ src/environments/start-world.ts
@@ -1,30 +1,38 @@
-import { Vector3, Scene } from '@babylonjs/core';
+import { Vector3, Scene, TransformNode } from '@babylonjs/core';
 import { World } from './world.js';
 import { EntityManager } from '../managers/entity-manager.js';
-import { FloorTileBasicEntity } from '../entities/floor-entity.js';
-import { RoofTileEmptyEntity } from '../entities/roof-entity.js';
-import { DoorDoubleWallSide } from '../entities/wall-entity.js';
+import * as models from '../entities/model.js';
 
 export class StartWorld extends World {
     constructor(scene: Scene, entityManager: EntityManager) {
         super(scene, entityManager);
-        this.buildFloorAndRoof();
-        this.buildWalls();
+        this.buildFloorAndRoof(scene, entityManager);
+        this.buildWalls(scene, entityManager);
     }
 
-    private buildWalls = () => {
-        const wall = new DoorDoubleWallSide(`wall`, this.scene, this.entityManager);
+    private buildWalls = (scene: Scene, entityManager: EntityManager) => {
+        const wallParent = new TransformNode('wall', scene);
+        const width = 10;
+        for (let x = 0; x < width; x++) {
+            const wall = models.windowWallSideB({ scene, entityManager });
+            wall.transform.position = new Vector3(x * 4 - width * 2 + 1, 0, 20);
+            wall.transform.parent = wallParent;
+        }
     };
 
-    private buildFloorAndRoof = () => {
+    private buildFloorAndRoof = (scene: Scene, entityManager: EntityManager) => {
+        const roofParent = new TransformNode('roof', scene);
+        const floorParent = new TransformNode('floor', scene);
         const width = 20;
         const height = 20;
         for (let x = 0; x < width; x++) {
             for (let z = 0; z < height; z++) {
-                const floor = new FloorTileBasicEntity(`floor-${x}-${z}`, this.scene, this.entityManager);
+                const floor = models.floorBasic1({ scene, entityManager });
                 floor.transform.position = new Vector3(x * 2 - width, 0, z * 2 - height);
-                const roof = new RoofTileEmptyEntity(`roof-${x}-${z}`, this.scene, this.entityManager);
+                floor.transform.parent = floorParent;
+                const roof = models.roofEmpty({ scene, entityManager });
                 roof.transform.position = new Vector3(x * 2 - width, 4, z * 2 - height);
+                roof.transform.parent = roofParent;
             }
         }
     };
--- src/states/start-state.ts
+++ src/states/start-state.ts
@@ -1,37 +1,39 @@
-import { Vector3, DirectionalLight, Color3, ShadowGenerator, SpotLight } from '@babylonjs/core';
+import { HemisphericLight, Vector3 } from '@babylonjs/core';
 import { SpaceSkybox } from '../skyboxes/space-skybox.js';
 import { IntroSound } from '../sounds/intro-sound.js';
 import { State } from './state.js';
-import { EntityManager } from '../managers/entity-manager.js';
 import { StartWorld } from '../environments/start-world.js';
 import { FirstPersonController } from '../controllers/first-person-controller.js';
+import * as models from '../entities/model.js';
 
 export class StartState extends State {
     run = async (): Promise<void> => {
+        const { scene, entityManager } = this;
         const framesPerSecond = 60;
         const gravity = -9.81;
-        this.scene.gravity = new Vector3(0, gravity / framesPerSecond, 0);
-        this.scene.collisionsEnabled = true;
-        this.scene.onPointerDown = (event) => {
-            if (event.button === 0) this.scene.getEngine().enterPointerlock();
-            if (event.button === 1) this.scene.getEngine().exitPointerlock();
+        scene.gravity = new Vector3(0, gravity / framesPerSecond, 0);
+        scene.collisionsEnabled = true;
+        scene.onPointerDown = (event) => {
+            if (event.button === 0) scene.getEngine().enterPointerlock();
+            if (event.button === 1) scene.getEngine().exitPointerlock();
         };
 
-        const entityManager = new EntityManager(this.assetManager);
-        new StartWorld(this.scene, entityManager);
-        const loadAssets = entityManager.load();
+        new StartWorld(scene, entityManager);
+        const doorway = models.doorDoubleWall({ scene, entityManager });
+        const loadAssets = this.entityManager.load();
 
-        new FirstPersonController(this.scene);
+        new FirstPersonController(scene);
 
         // const dL = new DirectionalLight('directionalLight', new Vector3(1, 0, 1), this.scene);
         // dL.diffuse = new Color3(1, 0, 0);
         // dL.specular = new Color3(1, 1, 0);
         // dL.intensity = 0.05;
         // new ShadowGenerator(1024, dL);
-        new IntroSound(this.scene);
-        // new HemisphericLight('light1', new Vector3(0, 1, 0), this.scene);
-        new SpaceSkybox(this.scene);
+        new IntroSound(scene);
+        new HemisphericLight('hemispheric-light', new Vector3(0, 1, 0), this.scene);
+        new SpaceSkybox(scene);
 
         await loadAssets;
+        doorway.doors.openAsync(true);
     };
 }
