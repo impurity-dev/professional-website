8c366b714bb7a626a76f4607c5cafd803fdfa958
impurity-dev <techkoko77@gmail.com>
2024-06-09 13:33:13 -0400

    Add animation for menu

--- src/guis/menu-gui.ts
+++ src/guis/menu-gui.ts
@@ -26,8 +26,8 @@ export class MenuGui {
         this.gui = this.createGui();
         this.title = this.createTitle();
         this.start = this.createStart(props.onStart);
-        this.optionMenu = this.createOptionMenu();
-        this.options = this.createOptions(this.optionMenu);
+        const { openOptionsMenu } = this.createOptionMenu();
+        this.options = this.createOptions(openOptionsMenu);
         this.camera = this.createCamera();
         this.menuClick = new MenuClickSound(this.scene);
         this.menuHover = new MenuHoverSound(this.scene);
@@ -91,7 +91,7 @@ export class MenuGui {
         return button;
     };
 
-    private createOptions = (submenu: GUI.Grid): GUI.Button => {
+    private createOptions = (openOptionsMenu: () => void): GUI.Button => {
         const button = GUI.Button.CreateImageWithCenterTextButton('options', 'Options', './gui/Blue/ButtonB_Big/Button6.png');
         button.height = '40px';
         button.width = '200px';
@@ -104,14 +104,64 @@ export class MenuGui {
         button.onPointerEnterObservable.add(() => this.menuHover.play());
         button.onPointerDownObservable.add(() => {
             this.menuClick.play();
-            submenu.alpha = 1;
+            openOptionsMenu();
         });
         this.gui.addControl(button);
         return button;
     };
 
-    private createOptionMenu = (): GUI.Grid => {
+    private createOptionMenu = (): {
+        grid: GUI.Grid;
+        background: GUI.Image;
+        openOptionsMenu: () => void;
+        closeOptionsMenu: () => void;
+    } => {
+        const width = 0.5;
+        const height = 0.7;
+        const animationSpeed = 5;
+        const startLocation = 1000;
+
+        const background = new GUI.Image('menu', '/gui/Blue/Panels/Panel6.png');
+        background.width = width;
+        background.height = height;
+        background.zIndex = 1;
+        background.top = startLocation;
+
         const grid = new GUI.Grid('grid');
-        return grid;
+        // grid.background = 'red';
+        grid.width = width;
+        grid.height = height;
+        grid.zIndex = 1;
+        grid.top = startLocation;
+
+        const flyIn = new BABYLON.Animation('fly-in', 'top', 60, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT);
+        flyIn.setKeys([
+            { frame: 0, value: startLocation },
+            { frame: 60, value: 0 },
+        ]);
+
+        const flyOut = new BABYLON.Animation('fly-out', 'top', 60, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT);
+        flyOut.setKeys([
+            { frame: 0, value: 0 },
+            { frame: 60, value: startLocation },
+        ]);
+
+        const openOptionsMenu = () => {
+            background.animations = [flyIn];
+            grid.animations = [flyIn];
+            this.scene.beginAnimation(grid, 0, 60, false, animationSpeed);
+            this.scene.beginAnimation(background, 0, 60, false, animationSpeed);
+        };
+
+        const closeOptionsMenu = () => {
+            background.animations = [flyOut];
+            grid.animations = [flyOut];
+            this.scene.beginAnimation(grid, 0, 60, false, animationSpeed);
+            this.scene.beginAnimation(background, 0, 60, false, animationSpeed);
+        };
+
+        this.gui.addControl(background);
+        this.gui.addControl(grid);
+        return { grid, background, openOptionsMenu, closeOptionsMenu };
     };
 }
