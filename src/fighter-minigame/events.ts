dc46a1c56d3bc4cac7d22e324d2a59742be718f4
impurity-dev <techkoko77@gmail.com>
2024-07-01 19:53:55 -0400

    Update events

--- src/fighter-minigame/events.ts
+++ src/fighter-minigame/events.ts
@@ -1,11 +1,11 @@
 import * as BABYLON from '@babylonjs/core';
 
 export class FighterEvents {
-    readonly movement: BABYLON.Observable<{
+    readonly controls: BABYLON.Observable<{
         pitch: number;
         yaw: number;
-        thrust: number;
+        movement: number;
+        boost: number;
     }> = new BABYLON.Observable();
     readonly fire: BABYLON.Observable<void> = new BABYLON.Observable();
-    readonly boost: BABYLON.Observable<void> = new BABYLON.Observable();
 }
--- src/fighter-minigame/inputs.ts
+++ src/fighter-minigame/inputs.ts
@@ -1,118 +1,90 @@
 import * as BABYLON from '@babylonjs/core';
-import { Model } from '../entities/model';
 import { FighterEvents } from './events';
 
+type MouseState = { current: BABYLON.IMouseEvent; last?: BABYLON.IMouseEvent } | undefined;
 export class FighterController {
     private readonly deviceManager: BABYLON.DeviceSourceManager;
-    private mouseState: { current: BABYLON.IMouseEvent; last?: BABYLON.IMouseEvent } = undefined;
+    private readonly events: FighterEvents;
 
-    constructor(props: { scene: BABYLON.Scene; target: Model; events: FighterEvents }) {
-        const { scene, target, events } = props;
+    constructor(props: { scene: BABYLON.Scene; events: FighterEvents }) {
+        const { scene, events } = props;
+        this.events = events;
         this.deviceManager = new BABYLON.DeviceSourceManager(scene.getEngine());
-        const { deviceManager } = this;
         scene.onPointerDown = (event) => {
             const engine = scene.getEngine() as BABYLON.Engine;
             if (event.button === 0) engine.enterPointerlock();
             if (event.button === 1) engine.exitPointerlock();
         };
+        let mouseState = undefined;
         scene.onPointerObservable.add((pointerInfo) => {
             switch (pointerInfo.type) {
                 case BABYLON.PointerEventTypes.POINTERDOWN:
                     // POINTERDOWN: capture the current mouse position as the `down` property
                     // Also capture it as the `last` property (effectively a drag of 0 pixels)
-                    this.mouseState = { current: pointerInfo.event, last: pointerInfo.event };
+                    mouseState = { current: pointerInfo.event, last: pointerInfo.event };
                     break;
                 case BABYLON.PointerEventTypes.POINTERUP:
                     // POINTERUP: drag has finished, so clear the mouse state
-                    this.mouseState = undefined;
+                    mouseState = undefined;
                     break;
                 case BABYLON.PointerEventTypes.POINTERMOVE:
                     // POINTERMOVE: while dragging, keep the `down` drag position
                     // but continuously update the `last` drag position
-                    if (this.mouseState) {
-                        this.mouseState.last = pointerInfo.event;
+                    if (mouseState) {
+                        mouseState.last = pointerInfo.event;
                     }
                     break;
             }
         });
-        target.transform.rotationQuaternion = BABYLON.Quaternion.Identity();
-        deviceManager.onDeviceConnectedObservable.add((device) => {
-            if (device.deviceType === BABYLON.DeviceType.Mouse) {
-                device.onInputChangedObservable.add((event) => this.onMouse(event, scene, target));
-            }
-            if (device.deviceType === BABYLON.DeviceType.Keyboard) {
-                device.onInputChangedObservable.add((event) => this.onKeyboard(event, scene, target));
-            }
+        this.deviceManager.onDeviceConnectedObservable.add((device) => {
+            if (device.deviceType !== BABYLON.DeviceType.Keyboard) return;
+            scene.onBeforeRenderObservable.add(() => {
+                const aimInputs = this.aim(scene, mouseState);
+                const moveInputs = this.move(device);
+                events.controls.notifyObservers({
+                    pitch: aimInputs?.pitch || 0,
+                    yaw: aimInputs?.yaw || 0,
+                    movement: moveInputs.movement,
+                    boost: moveInputs.boost,
+                });
+            });
         });
     }
 
-    onKeyboard = (event: BABYLON.IKeyboardEvent, scene: BABYLON.Scene, target: Model) => {};
-
-    onMouse = (event: BABYLON.IPointerEvent | BABYLON.IWheelEvent, scene: BABYLON.Scene, target: Model) => {
-        // Our maximum acceleration (units per second per second)
-        const MaxThrust = 10;
-        // Our maximum turn speed (radians per second)
-        const TurnSpeed = 1;
-        // The drag coefficient; roughly, how much velocity we will
-        // lose per second. Lower values means less drag and a more
-        // realistic "newtonian physics" feel, but may not be great
-        // for gameplay.
-        const DragCoefficient = 0.25;
-        // The ship's current velocity
-        const velocity = new BABYLON.Vector3();
-        const deltaSecs = scene.getEngine().getDeltaTime() / 1000;
-        const mouseInput = this.mouseInputs(scene);
-        if (mouseInput) {
-            // Convert Yaw and Pitch to a rotation in quaternion form
-            const turn = BABYLON.Quaternion.RotationYawPitchRoll(mouseInput.yaw * deltaSecs * TurnSpeed, mouseInput.pitch * deltaSecs * TurnSpeed, 0);
-            // Apply the rotation to our current rotation
-            target.transform.rotationQuaternion.multiplyInPlace(turn);
+    move = (keyboard: BABYLON.DeviceSource<BABYLON.DeviceType.Keyboard>) => {
+        const SPEED = 2;
+        const W = 87;
+        const S = 83;
+        const LeftShift = 16;
+        let movement = 0;
+        let boost = 1;
+        if (keyboard.getInput(W) === 1) {
+            if (keyboard.getInput(LeftShift) === 1) boost += 0.5;
+            movement += SPEED;
         }
-
-        // If we have input, compute acceleration, otherwise it's zero
-        const acceleration = mouseInput ? target.transform.forward.scale(mouseInput.thrust * MaxThrust * deltaSecs) : BABYLON.Vector3.Zero();
-
-        // Now apply the various physics forces to move the spaceship
-
-        // Apply acceleration to velocity
-        velocity.addInPlace(acceleration);
-        // Apply drag to dampen velocity
-        velocity.scaleInPlace(1 - DragCoefficient * deltaSecs);
-        // Apply velocity to position
-        target.transform.position.addInPlace(velocity.scale(deltaSecs));
+        if (keyboard.getInput(S) === 1) {
+            movement -= SPEED;
+        }
+        return { movement, boost };
     };
 
-    mouseInputs = (scene: BABYLON.Scene) => {
-        // If the mouse isn't being pressed, return null, which means "no input"
-        const { mouseState } = this;
-        if (!mouseState) {
-            return null;
-        }
-
+    aim = (scene: BABYLON.Scene, state: MouseState) => {
+        if (!state) return null;
         // Get the smallest of the screen's width or height
         const screenSize = Math.min(scene.getEngine().getRenderWidth(), scene.getEngine().getRenderHeight());
-
         // From the screen size, define a box that is 25% of the size
         // of the screen - this is effectively the max drag range of
         // the mouse drag, from the start point, in all 4 directions
         const dragSize = 0.25 * screenSize;
-
         // Compute the drag difference from the starting position of the drag
-        const dragX = mouseState.last.clientX - mouseState.current.clientX;
+        const dragX = state.last.clientX - state.current.clientX;
         // Note: +X maps to +Yaw, but +Y maps to -Pitch, so invert Y:
-        const dragY = mouseState.current.clientY - mouseState.last.clientY;
-
+        const dragY = state.current.clientY - state.last.clientY;
         // Normalised the values to [-1, 1] and map them like this:
         // * X maps to yaw (turn left/right)
         // * Y maps to pitch (turn up/down)
         const yaw = BABYLON.Scalar.Clamp(dragX / dragSize, -1, 1);
         const pitch = BABYLON.Scalar.Clamp(dragY / dragSize, -1, 1);
-
-        // Finally, return the mouse state in terms of spaceship controls
-        return {
-            thrust: 1,
-            yaw,
-            pitch,
-        };
+        return { pitch, yaw };
     };
 }
