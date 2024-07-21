1f2ff02b714b1a464aee40239e8b183bd430b53f
impurity-dev <techkoko77@gmail.com>
2024-07-21 07:52:49 -0400

    extract credits to seperate module

---
+++ src/credits-scene/credit-items.ts
@@ -0,0 +1,40 @@
+import * as BABYLON from '@babylonjs/core';
+import * as em from '../managers/entity-manager';
+import * as models from '../shared/models';
+import { CaroselItem } from './carosels';
+
+type GetItemProps = { scene: BABYLON.Scene; entityManager: em.EntityManager };
+export const creditItems: (x: GetItemProps) => CaroselItem[] = (props: GetItemProps) => {
+    const { scene, entityManager } = props;
+    return [
+        {
+            name: 'fighter',
+            link: 'https://sketchfab.com/3d-models/light-fighter-spaceship-free-51616ef53af84fe595c5603cd3e0f3e1',
+            model: new models.Model({
+                name: 'fighter',
+                scene,
+                entityManager,
+                asset: { file: 'fighter.glb', directory: 'assets/fighter/' },
+            }),
+            credits:
+                '"LIGHT FIGHTER SPACESHIP - FREE" by Kerem Kavalci. https://sketchfab.com/3d-models/light-fighter-spaceship-free-51616ef53af84fe595c5603cd3e0f3e1',
+        },
+        {
+            name: 'chamber',
+            link: 'https://skfb.ly/oODEM',
+            model: (() => {
+                const model = new models.Model({
+                    name: 'chamber',
+                    scene,
+                    entityManager,
+                    asset: { file: 'chamber_1k.glb', directory: 'assets/chamber/' },
+                });
+                model.transform.scaling = new BABYLON.Vector3(0.005, 0.005, 0.005);
+                model.transform.position = new BABYLON.Vector3(0, 0, -2.5);
+                return model;
+            })(),
+            credits:
+                '"Cloning Tank Chamber JFG - Roblox PBR Showcase" (https://skfb.ly/oODEM) by Jesus Fernandez Garcia is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).',
+        },
+    ];
+};
