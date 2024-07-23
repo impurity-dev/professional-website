71f01732c9ab31a328c8c49592bef0dc1dc2febc
impurity-dev <techkoko77@gmail.com>
2024-07-23 10:28:47 -0400

    Remove the road from station

--- src/character-scene/worlds.ts
+++ src/character-scene/worlds.ts
@@ -1,6 +1,5 @@
 import * as BABYLON from '@babylonjs/core';
-import { tap } from 'rxjs';
-import * as models from '../models';
+import * as sharedModels from '../models';
 import * as em from '../models/entity-manager.js';
 import { World } from '../shared/world.js';
 
@@ -9,6 +8,7 @@ export class CharacterWorld extends World {
         const { scene, entityManager } = props;
         super(scene, entityManager);
         this.lights({ scene });
+        this.station({ scene, entityManager });
         this.characters({ scene, entityManager });
     }
 
@@ -20,16 +20,40 @@ export class CharacterWorld extends World {
         light.specular = new BABYLON.Color3(1, 1, 1);
     };
 
+    private station = (props: { scene: BABYLON.Scene; entityManager: em.EntityManager }) => {
+        const { scene, entityManager } = props;
+        return sharedModels.station.station({ scene, entityManager });
+    };
+
     private characters = (props: { scene: BABYLON.Scene; entityManager: em.EntityManager }) => {
         const { scene, entityManager } = props;
-        models.station.station({ scene, entityManager });
-        const m = models.femaleCharaters.adventurer({ scene, entityManager });
-        m.onLoad
-            .pipe(
-                tap(() => {
-                    m.animationGroups.find((n) => n.name === 'Idle').play(true);
-                }),
-            )
-            .subscribe();
+        const characters = {
+            male: {
+                adventurer: sharedModels.maleCharaters.adventurer({ scene, entityManager }),
+                beach: sharedModels.maleCharaters.beach({ scene, entityManager }),
+                casual: sharedModels.maleCharaters.casual({ scene, entityManager }),
+                farmer: sharedModels.maleCharaters.farmer({ scene, entityManager }),
+                hoodie: sharedModels.maleCharaters.hoodie({ scene, entityManager }),
+                king: sharedModels.maleCharaters.king({ scene, entityManager }),
+                punk: sharedModels.maleCharaters.punk({ scene, entityManager }),
+                spacesuit: sharedModels.maleCharaters.spacesuit({ scene, entityManager }),
+                suit: sharedModels.maleCharaters.suit({ scene, entityManager }),
+                swat: sharedModels.maleCharaters.swat({ scene, entityManager }),
+                worker: sharedModels.maleCharaters.worker({ scene, entityManager }),
+            },
+            female: {
+                adventurer: sharedModels.femaleCharaters.adventurer({ scene, entityManager }),
+                casual: sharedModels.femaleCharaters.casual({ scene, entityManager }),
+                formal: sharedModels.femaleCharaters.formal({ scene, entityManager }),
+                medieval: sharedModels.femaleCharaters.medieval({ scene, entityManager }),
+                punk: sharedModels.femaleCharaters.punk({ scene, entityManager }),
+                sciFi: sharedModels.femaleCharaters.sciFi({ scene, entityManager }),
+                soldier: sharedModels.femaleCharaters.soldier({ scene, entityManager }),
+                suit: sharedModels.femaleCharaters.suit({ scene, entityManager }),
+                witch: sharedModels.femaleCharaters.witch({ scene, entityManager }),
+                worker: sharedModels.femaleCharaters.worker({ scene, entityManager }),
+            },
+        };
+        return characters;
     };
 }
