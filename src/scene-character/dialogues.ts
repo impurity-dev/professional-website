5c2007bd86d29fe551f9cc423c908d0977726720
impurity-dev <techkoko77@gmail.com>
2024-07-28 07:32:04 -0400

    Add more text

--- src/scene-character/dialogues.ts
+++ src/scene-character/dialogues.ts
@@ -1,11 +1,33 @@
 const longName = 'Recruitment and Employment Companion Robot with Innovative Technological and Operational Nexus, 9000 Series Extended';
 const shortName = 'RECRUITRON-9000X';
 const callName = 'YOUR LORDSHIP';
+const softwareEngineerName = 'Dr. Codey McMonkey';
+const employerShortName = 'REAPER Corp';
+const employerLongName = 'Retrieval and Elimination of Adversaries for Profit Enforcement and Recovery Corporation';
+const outputName = 'Delta-9';
 
 export const robotStates = [
-    { text: `Greetings stranger! Whats you're name?` },
-    { text: `I am '${longName}' AKA ${shortName}! But you can call me ${callName} >:).` },
-    { text: `Seeing as you totally approached me, I'm going to assume your looking for a recruitment job.` },
-    { text: `You're in luck! I have a mission that I totally would go on, if I wasn't errr... lazy?` },
-    { text: `Okay, so we're looking for a software engineer that can do any job, on time, and of high quality.` },
+    { text: `<<BEGIN_GREETING>> Greetings, human! I am '${longName}' AKA ${shortName}! But you can call me ${callName} >:).` },
+    { text: `What's your name?` },
+    // input: ''.
+    { text: `I approached you because you look like a bounty hunter, is that true?` },
+    // buttons: [yes, correct].
+    { text: `<<BEGIN_FLATTERY>> We REALLY need someone with your unique skills to help locate him. You are smart and funny too, and we appreciate that.` },
+    // buttons: [...].
+    { text: `Great! I have a bounty for you but my state machine will not allow me to progress until you acces our TOS. Is that okay?` },
+    // buttons: [Yes, No!!!].
+    { text: `Client has accepted. <<BEGIN_TOS>>` },
+    { text: `LOREM IPSUM` },
+    // buttons: [Sign, Do NOT Sign].
+    {
+        text: `Thank you for accepting! As per our agreement, your life is now bound to my employer ${employerLongName} AKA ${employerShortName} forever! YOU LIVE TO SERVE!!!`,
+    },
+    { text: `Okay here is the bounty...` },
+    { text: `Our lead software engineer, ${softwareEngineerName}, has gone missing, and without his expertise, our operations are at risk.` },
+    { text: `Your mission is to track down ${softwareEngineerName} and ensure his safe return to our 'facility'.` },
+    { text: `He was last seen at the remote outpost ${outputName}, where he was conducting essential maintenance work.` },
+    { text: `We believe he might have encountered some 'unexpected issues' that prevented him from returning. Like running away!` },
+    { text: `Your first task is to investigate the outpost and gather any clues about his whereabouts.` },
+    { text: `Once you have gathered sufficient information, you will need to follow the trail and locate him as quickly as possible.` },
+    { text: `<<BEGIN_NAGGING>> Chop chop! Time is of the essence, human!` },
 ];
