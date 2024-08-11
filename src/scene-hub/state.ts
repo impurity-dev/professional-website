d82e448f5338e8674af125aa1944666feeafee9d
impurity-dev <techkoko77@gmail.com>
2024-08-11 11:03:33 -0400

    Fix loc

--- src/scene-hub/state.ts
+++ src/scene-hub/state.ts
@@ -5,7 +5,6 @@ import * as worlds from './worlds.js';
 import * as controllers from './inputs.js';
 import * as guis from './guis.js';
 import * as localEvents from './events.js';
-import * as sounds from './sounds.js';
 import * as assets from './assets';
 import { filter, take, tap } from 'rxjs';
 
@@ -15,10 +14,9 @@ export class State extends states.State {
     build = async () => {
         const { scene, start$, destroy$, assetFactory } = this;
         const events = new localEvents.Events({ start$, destroy$ });
-        new controllers.FPSController({ scene, location: new BABYLON.Vector3(-20, 0, 1), target: new BABYLON.Vector3(1, 0, 0), events });
+        new controllers.FPSController({ scene, location: new BABYLON.Vector3(0, 0, 0), target: new BABYLON.Vector3(1, 0, 0), events });
         worlds.world({ assetFactory, events });
         guis.gui({ scene, events });
-        // sounds.trailerMusic({ scene });
         skyboxes.purpleSpace({ scene });
         events.actions$
             .pipe(
